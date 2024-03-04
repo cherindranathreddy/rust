8f5cb7532ca0f3515f0842576d61a62bca1c827e
cherindranath reddy <cherindranath.reddy@juspay.in>
2024-03-04 11:39:52 +0530

    cargo hello_world

---
+++ rust_lang/hello_cargo/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    println!("cargo new <folder_name>");
+    println!("cargo build && ./target/debug/<folder_name>");
+    println!("cargo run");
+    println!("cargo check");
+}
